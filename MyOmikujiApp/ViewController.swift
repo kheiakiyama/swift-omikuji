89e3fefbffd993c1b047c8579e1819d87f6532a6
kheiakiyama <kheiakiyama@gmail.com>
2015-04-12 21:45:07 +0900

    complete lesson

--- MyOmikujiApp/ViewController.swift
+++ MyOmikujiApp/ViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBAction func getOmikuji(sender: AnyObject) {
+        let results = [
+            "大吉",
+            "中吉",
+            "小吉",
+            "凶",
+            "大凶",
+        ]
+        let random = arc4random_uniform(UInt32(results.count))
+        if random == 0 {
+            self.myLabel.textColor = UIColor.redColor()
+        } else {
+            self.myLabel.textColor = UIColor.blackColor()
+        }
+        
+        self.myLabel.text = results[Int(random)]
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
